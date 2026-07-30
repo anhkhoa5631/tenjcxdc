04a199e794d67d196ff5570dba69103f146d3cf9
anhkhoa5631 <anhkhoa5631@gmail.com>
2026-07-30 10:16:14 +0700

    Add files via upload

---
+++ display.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        </head>
+    <body>
+        <h2> Data on form will be display on browser through PHP</h2>
+    <?php
+    echo "username: " . $_GET['username'] . "<br>";
+    echo "password: " . $_GET['password'] ; ? >
+</body>
+</html>
